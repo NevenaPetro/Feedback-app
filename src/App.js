cabe8b09f279a73f24d48d0f83995a80736a5b2c
Nevena.Petro <a.nevena.petrovic@gmail.com>
2023-01-28 20:36:05 +0100

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Header from './components/Header';
+
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import AboutPage from './pages/AboutPage';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import AboutIcon from './components/AboutIcon';
 import { FeedbackProvider } from './context/FeedbackContext';
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { StrictMode } from 'react';
-import ReactDOM, { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';

8ce8f1819e89addc91941749746962f93237e229
Nevena.Petro <a.nevena.petrovic@gmail.com>
2023-01-29 17:51:10 +0100

    finished

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,17 +1,19 @@
 import FeedbackItem from './FeedbackItem';
 import React from 'react';
-
+import Spinner from './shared/Spinner';
 import { motion, AnimatePresence } from 'framer-motion';
 import { useContext } from 'react';
 import FeedbackContext from '../context/FeedbackContext';
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
-  if (!feedback || feedback.length === 0) {
+  const { feedback, isLoading } = useContext(FeedbackContext);
+  if (!isLoading && (!feedback || feedback.length) === 0) {
     return <p>No Feedback yet</p>;
   }
 
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => (

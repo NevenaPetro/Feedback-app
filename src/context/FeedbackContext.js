09c13fcb41f133f8ad6c73efae16a222af289137
Nevena.Petro <a.nevena.petrovic@gmail.com>
2023-01-29 15:16:20 +0100

    server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,29 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This is feedback item 1',
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: 'This is feedback item 2',
-      rating: 7,
-    },
-    {
-      id: 3,
-      text: 'This is feedback item 3',
-      rating: 5,
-    }
-  ]);
+  const [isLoading, setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([]);
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
 
-const [feedbackEdit, setFeedbackEdit] = useState({
-  item: {},
-  edit: false,
-})
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  // Fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      'http://localhost:5000/feedback?_sort=id&order=desc'
+    );
+    const data = await response.json();
+    setFeedback(data);
+    setIsLoading(false);
+  };
 
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
@@ -38,27 +36,33 @@ const [feedbackEdit, setFeedbackEdit] = useState({
     }
   };
 
-//Update feedback
-const updateFeedback = (id, updItem) => {
-  setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updItem} : item))
-}
+  //Update feedback
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    );
+  };
 
   // This will set items to be updated
-const editFeedback = (item) => {
-  setFeedbackEdit({
-    item, 
-    edit: true
-  })
-}
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
 
   return (
-    <FeedbackContext.Provider value={{ 
-      deleteFeedback, 
-      addFeedback,
-      editFeedback,
-      updateFeedback,
-      feedback, 
-      feedbackEdit }}>
+    <FeedbackContext.Provider
+      value={{
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        updateFeedback,
+        isLoading,
+        feedback,
+        feedbackEdit,
+      }}
+    >
       {children}
     </FeedbackContext.Provider>
   );
